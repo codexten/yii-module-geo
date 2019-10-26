07deeedf69cf7b7b39fc6ec124073debadc48a18
aneena <aneenashaju@gmail.com>
2019-10-26 14:46:07 +0530

    corrections

--- src/controllers/DistrictController.php
+++ src/controllers/DistrictController.php
@@ -3,46 +3,10 @@
 namespace codexten\yii\modules\geo\controllers;
 
 use codexten\yii\modules\geo\models\District;
-use codexten\yii\modules\geo\models\Province;
-use Yii;
 
 class DistrictController extends BaseZoneController
 {
     public $modelClass = District::class;
 
-    /**
-     * @return array
-     */
-    public function actionDistrict()
-    {
-        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
-        $out = [];
-        if (isset($_POST['depdrop_parents'])) {
-            $parents = $_POST['depdrop_parents'];
-
-            if ($parents != null) {
-                $id = $parents[0];
-                $out = self::getDistrictList($id);
-                return ['output' => $out, 'selected' => ''];
-            }
-        }
-        return ['output' => '', 'selected' => ''];
-    }
-
-    /**
-     * @param $id
-     * @return array|District[]|\codexten\yii\modules\geo\models\Zone[]
-     */
-    public function getDistrictList($id)
-    {
-        $stateCode = Province::find()->select('code')->andWhere(['id' => $id])->one();
-
-        $districtList = District::find()
-            ->select([District::tableName().'.id', 'name'])
-            ->joinWith(['zoneGroup as zoneGroup'])
-            ->andWhere(['group_code' => $stateCode['code']])
-            ->asArray()->all();
-        return $districtList;
-    }
 
 }
